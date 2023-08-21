ec27f5deff642c6ab2e31e38793f95ca95bb8a0b
Rohit Sharma <rohit10012@gmail.com>
2023-08-21 08:05:12 +0530

    updated docker installation

--- docker-install.sh
+++ docker-install.sh
@@ -9,3 +9,6 @@ sudo chmod +x install-docker.sh
 sh install-docker.sh
 
 echo "Docker installed successfully"
+
+#add current user to root so that it can access docker socket
+sudo chown root:$USER /var/run/docker.sock
